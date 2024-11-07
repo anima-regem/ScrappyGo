8702d63f7e9d3ce9b1b5154241ec3a6f3673d2e9
anima-regem <vichukartha@gmail.com>
2024-11-07 20:10:00 +0530

    Implemented proper BFS

--- src/main.go
+++ src/main.go
@@ -4,68 +4,53 @@ import (
   "fmt"
   "github.com/gocolly/colly"
   "net/url"
-  
 )
 
 var links = make(map[string]bool)
-var linksArray = make([]string, 0)
+var linkQueue = make([]string, 0)
 
-func pushLink(link string){
-  fmt.Println("Pushed ", link)
-  if containsLink(link){
-    return
-  }
-  linksArray = append(linksArray, link)
+func enqueueLink(link string){
   links[link] = true
+  linkQueue = append(linkQueue, link)
 }
 
-func popLink() string {
-  fmt.Println("Popped ", linksArray[len(linksArray)-1])
-  length := len(linksArray)
-  if length == 0 {
-    return ""
-  } else {
-    link := linksArray[length -1]
-    linksArray = linksArray[:length -1]
-    return link
-  }
+func dequeueLink() string {
+  link := linkQueue[0]
+  linkQueue = linkQueue[1:]
+  return link
 }
 
-func containsLink(link string) bool {
-  _, ok := links[link]
-  return ok
-}
 
-func BFS(c *colly.Collector, root string){
-  c.Visit(root)
-  for len(linksArray) > 0 {
-    link := popLink()
+func BFS(c *colly.Collector, root string) {
+  enqueueLink(root)
+  for len(linkQueue) > 0 {
+    link := dequeueLink()
     fmt.Println(link)
-    BFS(c, link)
+    c.Visit(link)
   }
 }
 
-func main(){
+func main() {
   c := colly.NewCollector()
-
-  c.OnHTML("a[href]", func(e *colly.HTMLElement){
+  c.OnHTML("a[href]", func (e *colly.HTMLElement){
     link := e.Attr("href")
-    		parsedURL, err := url.Parse(link)
-		if err != nil || parsedURL.Host == "" {
-			return
-		}
+    link = e.Request.AbsoluteURL(link)
+    if link == "" || link == "#" {
+      return
+    }
+    u, err := url.Parse(link)
+    if err != nil {
+      return
+    }
+    if u.Scheme == "mailto" || u.Scheme == "tel" || u.Host == "" {
+      return
+    }
 
-		link = e.Request.AbsoluteURL(link)
-
-    parsedURL, _ = url.Parse(link)
-    link = parsedURL.Host
-    pushLink(link)
-
+    link = fmt.Sprintf("%s://%s", u.Scheme, u.Host)
+    if !links[link] {
+      enqueueLink(link)
+    }
   })
 
-  c.OnRequest(func(r *colly.Request){
-    fmt.Println("Visiting", r.URL)
-  })
-
-  BFS(c, "https://go.dev")
+  BFS(c, "https://blog.animaregem.me")
 }
